8ecc76f096dd51cf8dba4faac0280b71efe8fadc
manuelgutierrez02 <145865361+manuelgutierrez02@users.noreply.github.com>
2024-08-01 20:03:54 -0600

    Add files via upload

---
+++ salto-linea.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file salto-linea.php
+ * @version 1.0
+ * @author Víctor Cuervo (http://lineadecodigo.com)
+ * @date   08/febrero/2020
+ * @url    http://lineadecodigo.com/php/saltos-de-linea-en-php/
+ * @description Crear un salto de línea en una cadena
+ */
+
+
+echo "Principio de la frase \n";
+echo "Final de la frase \n";
+
+?>
